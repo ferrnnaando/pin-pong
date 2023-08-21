22451a2c60a0e1ec52e142619e59a0cc181045a5
Fernando Azuaga Svirina <ferrnnaando@gmail.com>
2023-08-21 15:37:32 +0200

    finish

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 #include <cstdlib>
 
 int contador_blue = 0;
@@ -8,9 +9,27 @@ int contador_red = 0;
 std::string WindowTitle = "Pin pong | @ferrnnaando";
 
 int main() {
+    //Create a window to charge all the loaded information
     sf::RenderWindow window(sf::VideoMode(250, 500), WindowTitle, sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
 
+    //Load sounds
+    sf::SoundBuffer collision_buffer;
+    if(!collision_buffer.loadFromFile("../build/required/sounds/redirect.mp3")) {
+        return -1;
+    }
+    sf::Sound collisionSound;
+    collisionSound.setBuffer(collision_buffer);
+
+    sf::SoundBuffer losedpoint_buffer;
+    if(!losedpoint_buffer.loadFromFile("../build/required/sounds/losed_point.mp3")) {
+        return -1;
+    }
+
+    sf::Sound losedpointSound;
+    losedpointSound.setBuffer(losedpoint_buffer);
+
+    //Load graphic fills
     sf::RectangleShape own_space;
     sf::RectangleShape opponent_space;
     own_space.setSize(sf::Vector2f((float)window.getSize().x, (float)window.getSize().y / 2));
@@ -20,6 +39,7 @@ int main() {
     opponent_space.setPosition(sf::Vector2f(0, (float)window.getSize().y / 2));
     opponent_space.setFillColor(sf::Color::Red);
 
+    //Load texts and fonts.
     sf::Font montserrat;
     if(!montserrat.loadFromFile("../build/required/fonts/montserrat/Montserrat-Regular.ttf")) {
         return -1;
@@ -37,11 +57,13 @@ int main() {
     counter_red.setPosition(window.getSize().x - 55, (window.getSize().y / 2) + 10);
     counter_red.setString("Points: " + std::to_string(contador_red));
 
+    //A line separator
     sf::Vertex line_wirefence[] = {
         sf::Vertex(sf::Vector2f(0, (float)window.getSize().y / 2)),
         sf::Vertex(sf::Vector2f(0, (float)window.getSize().y / 2))
     };
 
+    //Load textures and sprites to drawing for the rest of things
     sf::Texture paddle;
     if(!paddle.loadFromFile("../build/required/images/paddle.png")) {
         return -1;
@@ -101,6 +123,7 @@ int main() {
         //Colisiones
         if((paddle_sprite.getGlobalBounds().contains((float)ball_sprite.getPosition().x, (float)ball_sprite.getPosition().y))
         || (paddle2_sprite.getGlobalBounds().contains((float)ball_sprite.getPosition().x, (float)ball_sprite.getPosition().y))) {
+            collisionSound.play();
             ball_fastY *= -1;
         } 
         else if(ball_sprite.getPosition().y > window.getSize().y) {
@@ -108,12 +131,14 @@ int main() {
             ball_fastY *= -1;
             ball_sprite.setPosition(sf::Vector2f(rand() % 250, window.getSize().y / 2));
             counter_blue.setString("       " + std::to_string(contador_blue));
+            losedpointSound.play();
         }
         else if(ball_sprite.getPosition().y < 0) {
             contador_red++;
             ball_fastY *= -1;
             ball_sprite.setPosition(sf::Vector2f(rand() % 250, window.getSize().y / 2));
             counter_red.setString("       " + std::to_string(contador_red));
+            losedpointSound.play();
         }
 
         ball_sprite.move(ball_fastX, ball_fastY);
